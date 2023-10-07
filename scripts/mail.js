365742d8f1bfde562eeee1b8efaff56b82d37ab2
Agsaimo25 <147227917+Agsaimo25@users.noreply.github.com>
2023-10-07 13:02:36 +0530

    Update in Mail js

--- scripts/mail.js
+++ scripts/mail.js
@@ -2,33 +2,32 @@ document.getElementById("submit-btn").addEventListener("click", function(event) 
     event.preventDefault();
     
     var name = document.getElementById("name").value;
-    var email = document.getElementById("email").value;
+    var mobile = document.getElementById("mobile").value; // Updated to mobile
     var feedback = document.getElementById("feedback").value;
     
-    sendEmail(name, email, feedback);
+    sendFeedback(name, mobile, feedback);
 });
 
-function sendEmail(name, email, feedback) {
-   
-    emailjs.init("HBdFQ1htrZ0G9Y2ei");
+function sendFeedback(name, mobile, feedback) {
+    emailjs.init("YWfA4NXgppZDd1qQT");
     
     var params = {
         from_name: name,
-        from_email: email,
+        from_mobile: mobile, // Updated to mobile
         message: feedback
     };
     
-    emailjs.send("service_2koh6ji", "template_uuzrm77", params)
+    emailjs.send("service_l9uwmxt", "template_0c6iq2i", params)
         .then(function(response) {
-            console.log("Email sent:", response);
-            alert("Email sent successfully!");
+            console.log("Feedback sent:", response);
+            alert("Feedback sent successfully!");
 
-           
+            // Clear the form fields after successful submission
             document.getElementById("name").value = "";
-            document.getElementById("email").value = "";
+            document.getElementById("mobile").value = "";
             document.getElementById("feedback").value = "";
         }, function(error) {
-            console.error("Email error:", error);
-            alert("Error sending email.");
+            console.error("Feedback error:", error);
+            alert("Error sending feedback.");
         });
 }
